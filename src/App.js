7246aa97fb645a8de6691e13d6dd6ab8df8d167b
kainat kainat <kainathafeez04@gmail.com>
2024-04-18 23:07:59 -0700

    social

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [Thumbnail, setThumbnail] = useState("");
   const [videoQuality, setvideoQuality] = useState("");
   const [videoDownloadURL, setvideoDownloadURL] = useState("");
+  const [videoHidden, setVideoHidden] = useState(true);
   const [error, seterror] = useState(false);
 
 
@@ -22,7 +23,7 @@ function App() {
       },
       headers: {
         'X-RapidAPI-Key': '824dddee65msh76dbe040b14bfe4p12ab6ajsnc6532155d4f8',
-        'X-RapidAPI-Host': 'social-media-video-downloader.p.rapidapi.com'
+        'X-RapidAPI-Host': 'social-media-video-downloader .p.rapidapi.com'
       }
     };
 
@@ -39,6 +40,10 @@ function App() {
       seterror(true);
     }
   };
+  const hiddenBtn = async () => {
+    setVideoHidden(false)
+    return
+  }
 
 
 
@@ -66,12 +71,13 @@ function App() {
         />
         <button className="button" onClick={getApidata}>Download Video</button>
         {isLoading && <div className='text-black bg-transparent mt-5'>Loading...</div>}
-        {videoData && !error && <div className='flex flex-row justify-between border-2 border-indigo-800 items-center w-10/12 p-5 m-5'>
+        {videoData && !error && <div className='flex flex-row relative  justify-between border-2 border-indigo-800 items-center w-10/12 p-5 m-5'>
           <img src={Thumbnail} alt="" srcset="" className='h-20 w-20' />
           <h3 className='text-black'>{videoQuality}</h3>
           <a href={videoDownloadURL} target='_blank' rel='noreferrer' className='bg-blue-800 text-white p-2 rounded'> Download </a>
-        </div>}
+      </div>}
         {error && <div className='p-5 m-5 text-white bg-red-600 rounded'>Something went wrong...</div>}
+      
       </div>
     </div>
   );
